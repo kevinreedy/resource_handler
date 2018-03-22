476f5367705c5ae0c2343f083055b45972314444
Kevin Reedy <kevinreedy@gmail.com>
2018-03-22 16:35:53 -0500

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'kreedy@chef.io'
 license 'Apache-2.0'
 description 'Installs/Configures resource_handler'
 long_description 'Installs/Configures resource_handler'
-version '0.1.1'
+version '0.1.2'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 issues_url 'https://github.com/kevinreedy/resource_handler/issues'
 source_url 'https://github.com/kevinreedy/resource_handler'
